813aebaae8434b6802326ded7790636017168ad4
Ayhan Krala <codewolf27@gmail.com>
2023-11-20 18:26:25 +0300

    Implement DTOs with validation for entities Make, Model, Part, and PartCategory

---
+++ src/main/java/com/example/AutoPartsOnlineShopApi/dto/MakeRequestDTO.java
@@ -0,0 +1,27 @@
+package com.example.AutoPartsOnlineShopApi.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MakeRequestDTO {
+
+    @NotBlank(message = "Manufacturer name is required")
+    private String name;
+
+    private String countryOfOrigin;
+
+    @PositiveOrZero(message = "Founding year must be a positive or zero value")
+    private Integer foundingYear;
+
+    private String headquarters;
+
+    private boolean isLuxuryBrand;
+
+
+}
---
+++ src/main/java/com/example/AutoPartsOnlineShopApi/dto/MakeResponseDTO.java
@@ -0,0 +1,16 @@
+package com.example.AutoPartsOnlineShopApi.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MakeResponseDTO {
+
+    private Long id;
+    private String name;
+
+
+}
